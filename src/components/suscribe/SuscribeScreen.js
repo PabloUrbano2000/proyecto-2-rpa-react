c9c35ebd2786ccd8de4e4aacdb2ee52082903175
marcomiler <miler.rmarco@gmail.com>
2021-08-02 18:09:35 -0500

    se creo el archivo suscribe

---
+++ src/components/suscribe/SuscribeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SuscribeScreen = () => {
+    return (
+        <div>
+            <h1>SuscribeScreen</h1>
+        </div>
+    );
+}
+
+export default SuscribeScreen;
